2cb372d3c2bfb90bf4441c833dde74521034d4e4
Ali Taherzadeh <Ali.Thz83@gmail.com>
2024-04-03 09:02:57 +0330

    gson added

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import org.gradle.internal.impldep.com.google.gson.Gson
+
 plugins {
     id("java")
 }
@@ -12,6 +14,7 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("com.google.code.gson:gson:2.7")
 }
 
 tasks.test {
