d3c45d7e9601ca70f95e638c6788a16fc5678eda
Ali Taherzadeh <Ali.Thz83@gmail.com>
2024-04-03 12:11:57 +0330

    testing files

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,18 @@
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+
 import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        UserRepository userRepository = new UserRepository();
-        userRepository.insert(new User("bc","b", "c", 15, new Account("ali.thz83@gmail.com", "Ab!@#12345")));
-        ArrayList<User> users = userRepository.select();
+//        UserRepository userRepository = new UserRepository();
+//        userRepository.insert(new User("bc","b", "c", 15, new Account("ali.thz83@gmail.com", "Ab!@#12345")));
+//        ArrayList<User> users = userRepository.select();
+//        System.out.println();
+
+//        Gson gson = new Gson();
+//        String jsonObject = gson.toJson(new User("bc","b", "c", 15, new Account("ali.thz83@gmail.com", "Ab!@#12345")));
+//        User user = gson.fromJson(jsonObject, User.class);
+//        System.out.println(user.getFirstName());
     }
 }
